c00b3fcab12170264f6ba02ec4217e48960efc9d
Giora Guttsait <giora111@gmail.com>
2020-09-19 01:49:51 +0300

    Add cors to server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { schema } from './graphql-schema';
+import cors from 'cors';
 
 export const app = express();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
